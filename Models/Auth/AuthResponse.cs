1f3d43979a8c676bc5baced60c924f49fcc5e7c7
mesut <ergul_mesut@yahoo.com>
2025-05-26 00:31:13 +0300

    optim

---
+++ Models/Auth/AuthResponse.cs
@@ -0,0 +1,10 @@
+namespace MyTts.Models.Auth
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+        public UserInfo User { get; set; } = new();
+    }
+}
---
+++ Models/Auth/RefreshTokenRequest.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTts.Models.Auth
+{
+    public class RefreshTokenRequest
+    {
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+        [Required]
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
